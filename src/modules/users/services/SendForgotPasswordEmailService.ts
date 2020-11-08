d532af3a2e5c067e4cfa6bfd61ba9d2be1e77754
Wesley Fracanábia <wesleyf.santos92@hotmail.com>
2020-11-07 22:18:57 -0300

    feat: add engine template

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe'
 import AppError from '@shared/errors/AppError'
-
+import path from 'path'
 import IUsersRepository from '@modules/users/repositories/IUsersRepository'
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository'
@@ -29,10 +29,24 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id)
 
-    await this.mailProvider.sendMail(
-      email,
-      `Pedido de recupação de senha recebido: ${token}`
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs'
     )
+
+    await this.mailProvider.sendMail({
+      to: { name: user.name, email: user.email },
+      subject: '[GoBarber] Recuperação de senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/forgot-password/reset?token${token}`,
+        },
+      },
+    })
   }
 }
 
